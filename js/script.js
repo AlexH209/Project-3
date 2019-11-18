f1feb3d044169bd74da63cfb8f952b76fdfbdc45
AlexH209 <alexher209@gmail.com>
2019-11-18 07:37:47 -0800

    Commit

--- js/script.js
+++ js/script.js
@@ -169,18 +169,18 @@ let easyButton = document.getElementById('easyButton'),
     backButton = document.getElementById('backButton'),
     optionsScreen = document.getElementById('optionsScreen');
 
-//optionsButton.addEventListener('click', function (event) {
-//    menuButtons();
-//
-//    optionsScreen.innerHTML = '<div id="bgOptionsColor" class="bg-dark d-flex justify-content-center align-items-end pb-5 row" style="height: 100vh; width: 100vw; opacity: 75%;">' +
-//        '<div class="col-12"><a href="#" id="musicButton" class="col-2 btn text-dark bg-white border-dark rounded-0" style="font-size: 35px; opacity: 75%;">Music On/Off</a></div>' +
-//        '<div class="col-12"><a href="#" id="backMenuButton" class="col-2 btn text-dark bg-white border-dark rounded-0" style="font-size: 35px; opacity: 75%;">Back</a></div>'
-//
-//    backMenuButton.addEventListener('click', function (event) {
-//        optionsScreen.innerHTML = '';
-//
-//    });
-//});
+optionsButton.addEventListener('click', function (event) {
+    menuButtons();
+
+    optionsScreen.innerHTML = '<div id="bgOptionsColor" class="bg-dark d-flex justify-content-center align-items-end pb-5 row" style="height: 100vh; width: 100vw; opacity: 75%;">' +
+        '<div class="col-12"><a href="#" id="musicButton" class="col-2 btn text-dark bg-white border-dark rounded-0" style="font-size: 35px; opacity: 75%;">Music On/Off</a></div>' +
+        '<div class="col-12"><a href="#" id="backMenuButton" class="col-2 btn text-dark bg-white border-dark rounded-0" style="font-size: 35px; opacity: 75%;">Back</a></div>'
+
+    backMenuButton.addEventListener('click', function (event) {
+        optionsScreen.innerHTML = '';
+
+    });
+});
 
 function menuButtons(){
     easyButton.style.display = 'none';
